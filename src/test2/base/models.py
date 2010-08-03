99e1cb33d822f380b1020a34c609340a87d24d78
Chris Dew <cmsdew+github@gmail.com>
2010-08-03 14:02:40 +0100

    added Django path to Eclipse

--- src/test2/base/models.py
+++ src/test2/base/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
 class UserProfile(models.Model):
-    user = models.ForeignKey(User, unique=True)
-    info = models.C
+    info = models.Char
 
     class Meta:
         db_table = u'user_profile'
